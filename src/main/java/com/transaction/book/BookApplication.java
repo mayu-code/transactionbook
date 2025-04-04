c44f14a09e5edf3b6d42c24d7fe2d0607949f503
mayu_code <durbulemayu@gmail.com>
2025-04-04 16:42:16 +0530

    sync

--- src/main/java/com/transaction/book/BookApplication.java
+++ src/main/java/com/transaction/book/BookApplication.java
@@ -10,11 +10,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 @EnableScheduling
 public class BookApplication {
 
-	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		
+	public static void main(String[] args) {	
 		SpringApplication.run(BookApplication.class, args);
 	}
 
